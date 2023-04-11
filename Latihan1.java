a0538a4cc5df1531d9939a32affa63af08029da6
MufidatulNabilla <127195184+MufidatulNabilla@users.noreply.github.com>
2023-04-11 13:43:01 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,36 @@
+package Modul4;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Latihan1 {
+    String nama, kelas, matkulPraktikum;
+    int nim;
+
+    public Latihan1(String nm, String kl, String mat, int ni){
+        nama = nm;
+        kelas = kl;
+        matkulPraktikum = mat;
+        nim = ni;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        HashMap<String, Latihan1> mhs = new HashMap<>();
+        String input;
+        Latihan1 data;
+
+        mhs.put("1", new Latihan1("Puteri", "3H", "Struktur Data", 2020000));
+        mhs.put("2", new Latihan1("Agus", "3A", "Matematika", 2020012));
+        mhs.put("3", new Latihan1("Arro", "3D", "Pemrograman", 2020017));
+
+        System.out.println("Masukkan ID: ");
+        input = in.nextLine();
+        data = mhs.get(input);
+        if(data != null){
+            System.out.println("Data Mahasiswa : " + data.nama + ", kelas : " +
+                    data.kelas + ", mata kuliah praktikum : " + data.matkulPraktikum +
+                    ", nim : " + data.nim);
+        }
+    }
+}
